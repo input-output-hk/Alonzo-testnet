206e969d1d0d6831ea8ac7aaf85eb8d86024c7a5
Igodlab <igodlab@gmail.com>
2021-07-14 10:37:24 -0700

    Solution README for exercise1 finalized

--- Alonzo-solutions/exercise3/randDatumGen.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from numpy import random
-randDatum = ''
-for i in range(4):
-    randDatum += str(random.randint(1e3,1e4-1))
-
-with open("./txs/randomDatum.txt", "w") as f:
-    f.write(randDatum)
-print(randDatum)
