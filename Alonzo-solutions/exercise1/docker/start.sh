ec44fee5f820b1f40402f1ca700af82451ec4ccb
Zarko Jevtic <zarej@svrljig.net>
2021-08-18 18:01:57 +0200

    Updated exercise solution 1 docker for alonzo purple 1.0.2

--- Alonzo-solutions/exercise1/docker/start.sh
+++ Alonzo-solutions/exercise1/docker/start.sh
@@ -13,14 +13,14 @@ touch ./configuration/sockets/node.socket
 
 ##Getting Config
 echo "--getting config"
-wget  https://hydra.iohk.io/build/6510764/download/1/alonzo-blue-config.json -P ./configuration/config/
-wget  https://hydra.iohk.io/build/6510764/download/1/alonzo-blue-byron-genesis.json -P ./configuration/config/
-wget  https://hydra.iohk.io/build/6510764/download/1/alonzo-blue-shelley-genesis.json -P ./configuration/config/
-wget  https://hydra.iohk.io/build/6510764/download/1/alonzo-blue-alonzo-genesis.json -P ./configuration/config/
+wget   https://hydra.iohk.io/build/7189190/download/1/alonzo-purple-config.json -P ./configuration/config/
+wget  https://hydra.iohk.io/build/7189190/download/1/alonzo-purple-byron-genesis.json -P ./configuration/config/
+wget  https://hydra.iohk.io/build/7189190/download/1/alonzo-purple-shelley-genesis.json -P ./configuration/config/
+wget  https://hydra.iohk.io/build/7189190/download/1/alonzo-purple-alonzo-genesis.json -P ./configuration/config/
 
 ##Getting Topology
 echo "--getting topology"
-wget  https://hydra.iohk.io/build/6510764/download/1/alonzo-blue-topology.json -P ./configuration/topology/
+wget  https://hydra.iohk.io/build/7189190/download/1/alonzo-purple-topology.json -P ./configuration/topology/
 
 ##Starting Docker-Compose
 docker-compose up
