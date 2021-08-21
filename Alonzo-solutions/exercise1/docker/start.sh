15842d33a7c7df8abc485197a8f7fe773320904b
Zarko Jevtic <zarej@svrljig.net>
2021-08-21 10:13:01 +0200

    Updated exercise1 docker solution with docker image cardano-node:1.29.0-rc2

--- Alonzo-solutions/exercise1/docker/start.sh
+++ Alonzo-solutions/exercise1/docker/start.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+export NODE_TAG=1.29.0-rc2
+CONFIG=https://hydra.iohk.io/build/7366583/download/1/alonzo-purple-config.json
+BYRON_GENESIS=https://hydra.iohk.io/build/7366583/download/1/alonzo-purple-byron-genesis.json
+SHELLEY_GENESIS=https://hydra.iohk.io/build/7366583/download/1/alonzo-purple-shelley-genesis.json
+ALONZO_GENESIS=https://hydra.iohk.io/build/7366583/download/1/alonzo-purple-alonzo-genesis.json
+TOPOLOGY=https://hydra.iohk.io/build/7189190/download/1/alonzo-purple-topology.json
+
 ##Making some folders
 mkdir -p ./configuration/config/
 mkdir -p ./configuration/topology/
@@ -13,14 +20,14 @@ touch ./configuration/sockets/node.socket
 
 ##Getting Config
 echo "--getting config"
-wget   https://hydra.iohk.io/build/7189190/download/1/alonzo-purple-config.json -P ./configuration/config/
-wget  https://hydra.iohk.io/build/7189190/download/1/alonzo-purple-byron-genesis.json -P ./configuration/config/
-wget  https://hydra.iohk.io/build/7189190/download/1/alonzo-purple-shelley-genesis.json -P ./configuration/config/
-wget  https://hydra.iohk.io/build/7189190/download/1/alonzo-purple-alonzo-genesis.json -P ./configuration/config/
+wget  $CONFIG -P ./configuration/config
+wget  $BYRON_GENESIS -P ./configuration/config/
+wget  $SHELLEY_GENESIS -P ./configuration/config/
+wget  $ALONZO_GENESIS -P ./configuration/config/
 
 ##Getting Topology
 echo "--getting topology"
-wget  https://hydra.iohk.io/build/7189190/download/1/alonzo-purple-topology.json -P ./configuration/topology/
+wget $TOPOLOGY -P ./configuration/topology/
 
 ##Starting Docker-Compose
 docker-compose up
