e9d0b43d28e72e4e9f0e950b8117b095375b3ac2
ahaxu <banghahatinh96@gmail.com>
2021-10-29 15:26:26 +0700

    Fix CekError An error has occurred:  User error:

--- resources/plutus-sources/plutus-helloworld/app/plutus-helloworld-literal-bytestring.hs
+++ resources/plutus-sources/plutus-helloworld/app/plutus-helloworld-literal-bytestring.hs
@@ -14,21 +14,22 @@ main :: IO ()
 main = do
   args <- getArgs
   let nargs = length args
-  let scriptData = if nargs > 0 then fromString (args!!0) else "Hello World!"
+  let scriptData = if nargs > 0 then head args else "Hello World!"
   let scriptname = if nargs > 1 then args!!1 else  "result.plutus"
-  putStrLn $ "Writing output to: " ++ scriptname
-  writePlutusScript scriptData scriptname helloWorldSerialised helloWorldSBS
+  putStrLn $ "Writing output to: " ++ scriptname ++ " with scriptData " ++ scriptData
+  writePlutusScript (fromString scriptData) scriptname helloWorldSerialised helloWorldSBS
 
 writePlutusScript :: Plutus.BuiltinByteString -> FilePath -> PlutusScript PlutusScriptV1 -> SBS.ShortByteString -> IO ()
 writePlutusScript scriptData filename scriptSerial scriptSBS =
   do
   case Plutus.defaultCostModelParams of
         Just m ->
-          let (logout, e) = Plutus.evaluateScriptCounting Plutus.Verbose m scriptSBS
-                              [ Plutus.toData scriptData
-                              , Plutus.toData ()
-                              , Plutus.toData ()
-                              ]
+          let (logout, e) =
+                Plutus.evaluateScriptCounting
+                    Plutus.Verbose
+                    m
+                    scriptSBS
+                    [Plutus.toData scriptData]
           in do print ("Log output" :: String) >> print logout
                 case e of
                   Left evalErr -> print ("Eval Error" :: String) >> print evalErr
